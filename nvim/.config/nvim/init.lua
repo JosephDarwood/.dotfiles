f0e64136df4a3ce8806d4b14d3d969a41acdc8ca
Joseph <josephdarwood123@gmail.com>
2025-11-16 22:45:27 -0800

    some updates, issues with hyprland, cleaning up/developing the nvim setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.g.have_nerd_font = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.relativenumber = true
 vim.keymap.set("n", "<leader>ttfquick", function()
   print("Hello from <leader>!")
 end, { desc = "Base Leader cmd" })
